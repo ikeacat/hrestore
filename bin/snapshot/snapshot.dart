2b7e16f7fbb5c65c01e4a5fd72479c2cf0d9c50d
ikeacat <mfryk268@gmail.com>
2023-10-17 16:54:29 -0500

    scaffolding for drive snapshot!!!

--- bin/snapshot/snapshot.dart
+++ bin/snapshot/snapshot.dart
@@ -1,8 +1,12 @@
 import 'chrome.dart';
 import '../load_config.dart';
+import 'drive.dart';
 
 void snapshot() {
   if (ConfigStore.globalStore["SnapshotChromeData"] == "true") {
     chromeSnapshot();
+  }
+  if (ConfigStore.globalStore["SnapshotDriveLogin"] == "true") {
+    driveSnapshot();
   }
 }
