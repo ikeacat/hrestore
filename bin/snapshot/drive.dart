a7890e5552c755c037db6fccd4e680ca3b623040
ikeacat <mfryk268@gmail.com>
2023-10-21 11:10:19 -0500

    cleanup

--- bin/snapshot/drive.dart
+++ bin/snapshot/drive.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:sqlite3/sqlite3.dart';
 
@@ -19,7 +18,6 @@ List<String>? grabAccountIDs() {
 
   final expDBRows = db.select("SELECT * FROM PhenotypeValues");
 
-  int i = 0;
   for (Row row in expDBRows) {
     if (row[0] == "account_ids") {
       print(utf8.decode(row[1]).replaceAll("\n", "+").replaceFirst("+", ""));
