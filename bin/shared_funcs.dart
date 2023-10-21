dde2bd41e838d9a3db519093b64755becf05bd88
ikeacat <mfryk268@gmail.com>
2023-10-21 11:08:14 -0500

    get account ids

--- bin/shared_funcs.dart
+++ bin/shared_funcs.dart
@@ -1,6 +1,30 @@
+import 'dart:ffi';
 import 'dart:io';
+
+import 'package:sqlite3/open.dart';
 
 (int, dynamic, dynamic) copyDirectorySync(String from, String to) {
   final moveProc = Process.runSync("xcopy", ["/E", "/I", "/Y", from, to]);
   return (moveProc.exitCode, moveProc.stdout, moveProc.stderr);
+}
+
+bool killDriveProcess() {
+  var taskkill = Process.runSync(
+      "taskkill", ["/im", "GoogleDriveFS.exe", "/f", "/t"],
+      runInShell: true);
+  if (taskkill.exitCode != 0 && taskkill.exitCode != 128) {
+    // 128: proc not found
+    print("\tTaskkill code: ${taskkill.exitCode}");
+    print("\tTaskkill output: ${taskkill.stdout}");
+    print("\tTaskkill ERROR output: ${taskkill.stderr}");
+    stderr.write("Not sure if Google Drive was killed (non-zero exit code).");
+    return false;
+  }
+  return true;
+}
+
+void setupSqliteLibrary() {
+  open.overrideFor(OperatingSystem.windows, () {
+    return DynamicLibrary.open("lib/sqlite3.dll");
+  });
 }
--- bin/snapshot/drive.dart
+++ bin/snapshot/drive.dart
@@ -1,1 +1,31 @@
-void driveSnapshot() {}
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:sqlite3/sqlite3.dart';
+
+import '../shared_funcs.dart';
+
+void driveSnapshot() {
+  if (!killDriveProcess()) {
+    stdout.write(" Aborting.");
+    return;
+  }
+}
+
+List<String>? grabAccountIDs() {
+  final db = sqlite3.open(
+      "${Platform.environment["LOCALAPPDATA"]}\\Google\\DriveFS\\experiments.db");
+
+  final expDBRows = db.select("SELECT * FROM PhenotypeValues");
+
+  int i = 0;
+  for (Row row in expDBRows) {
+    if (row[0] == "account_ids") {
+      print(utf8.decode(row[1]).replaceAll("\n", "+").replaceFirst("+", ""));
+      return (utf8.decode(row[1]).trim().split("\n"));
+    }
+  }
+
+  return null;
+}
